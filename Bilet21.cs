eb86acbb82ea3e32418a5f9ddb9169360a2f4c72
sapok2030 <115653053+sapok2030@users.noreply.github.com>
2024-03-11 19:34:16 +0300

    Bilet21
    Bilet21

---
+++ Bilet21.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static double SumOfSequence(double[] sequence)
+    {
+        double sum = 0;
+        foreach (double element in sequence)
+        {
+            sum += element;
+        }
+        return sum;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите количество элементов в последовательности:");
+        int n = int.Parse(Console.ReadLine());
+        double[] sequence = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите {i + 1}-й элемент последовательности: ");
+            sequence[i] = double.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"Сумма всех элементов последовательности: {SumOfSequence(sequence)}");
+    }
+}
